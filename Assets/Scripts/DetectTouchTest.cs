47f66dce1d8b4f9f5369cbc4b5e85e3730028d67
audecaz <aude.cazottes@gmail.com>
2022-11-25 12:00:23 +0100

    Initial Commit 2
    Correction

---
+++ Assets/Scripts/DetectTouchTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectTouchTest : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider != null)
+                {
+                    Debug.Log(hit.collider.gameObject.name);
+                }
+            }
+        }
+    }
+}
