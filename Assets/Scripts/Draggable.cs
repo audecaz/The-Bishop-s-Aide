4f871364b3da40a81b639894a1a3d2d85107fc4c
audecaz <aude.cazottes@gmail.com>
2022-12-03 18:08:24 +0100

    Ajout slots drag and drop
    Drag and drop fonctionnel, les objets reviennent à leur point d'origine si trop loin du slot

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,19 @@ public class Draggable : MonoBehaviour
 {
     private bool drag;
     private Vector3 newPos;
+    private Vector3 originalPos;
     private float test;
+
+    public GameObject slot;
 
     private void Start()
     {
+        originalPos = transform.position;
         //test = transform.position.z;
-        newPos.z = transform.position.z + 10;
+        newPos.z = transform.position.z + 8;
+
     }
-    private void OnMouseDown()
+    private void OnMouseDown() //fonctionne pour le touch
     {
         //Debug.Log("tient");
         drag = true;
@@ -21,7 +27,14 @@ public class Draggable : MonoBehaviour
 
     private void OnMouseUp()
     {
-        //Debug.Log("lache");
+        if(Vector3.Distance(transform.position, slot.transform.position) < 8)
+        {
+            transform.position = slot.transform.position;
+        }
+        else
+        {
+            transform.position = originalPos;
+        }
         drag = false;
     }
 
@@ -29,6 +42,7 @@ public class Draggable : MonoBehaviour
     {
         if (drag)
         {
+            //Debug.Log(Vector3.Distance(transform.position, slot.transform.position));
             Touch FingerTouch = Input.GetTouch(0);
             //Debug.Log(FingerTouch.position.x);
             newPos.x = FingerTouch.position.x;
